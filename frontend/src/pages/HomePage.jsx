c1e8c3c3f2b35068386fdb1119bb4e838a49be2d
Swayam8115 <swayampatel2421@gmail.com>
2025-02-03 01:36:05 +0530

    Made NavBar Dynamic and added Layout

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import {Navbar} from "../components"
 export default function HomePage() {
   return (
     // background: linear-gradient(90deg, #FF1F01 0%, #FFC700 100%);
 
     <div className='gradient bg-gradient-to-l to-[#FF1F01] from-[#FFC700] w-screen h-screen'>
-    <Navbar/>
+    
     </div>
   )
 }
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,7 +1,19 @@
 import HomePage from "./HomePage.jsx";
 import Chatroom from "./Chatroom.jsx";
+import Community from "./Community.jsx";
+import About from "./About.jsx";
+import Leaderboard from "./Leaderboard.jsx";
+import Contactus from "./Contactus.jsx";
+import SignIn from "./SignIn.jsx";
+import SignUp from "./SignUp.jsx";
 
 export {
     HomePage,
-    Chatroom
+    Chatroom,
+    Community,
+    About,
+    Leaderboard,
+    Contactus,
+    SignIn,
+    SignUp
 }
