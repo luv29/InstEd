11e39ee32ecf39a270ce43e756600c86c911fc86
Swayam8115 <swayampatel2421@gmail.com>
2025-02-08 14:23:25 +0530

    Bug Fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,52 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import MainLayout from "./layout/MainLayout.jsx"
+import { Toaster } from "./components/ui/toaster"
+import RegisterLayout from "./layout/RegisterLayout.jsx"
+import {
+  HomePage,
+  Chatroom,
+  Community,
+  About,
+  Leaderboard,
+  Contactus,
+  SignIn,
+  SignUp,
+  SignupOtp,
+  SignUpInterests,
+  ProfilePage
+} from './pages'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<MainLayout />} >
+        <Route path='/' element={<HomePage />} />
+
+        <Route path='/profile' element={<ProfilePage />} />
+
+        <Route path='/signup' element={<RegisterLayout />} >
+          <Route path='' element={<SignUp />} />
+          <Route path='otp' element={<SignupOtp />} />
+          <Route path='interests' element={<SignUpInterests />} />
+        </Route>
+
+        <Route path='/chat' element={<Chatroom />} />
+        <Route path='/community' element={<Community />} />
+        <Route path='/about-us' element={<About />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
+        <Route path='/contact-us' element={<Contactus />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Route>
+    </>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+    <RouterProvider router={router} />
+    <Toaster />
   </StrictMode>,
 )
